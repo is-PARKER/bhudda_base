ed847ce78f98fa9d6fe1fd81d11a01d929eba422
C. Parker <Parker.chrisdavid@gmail.com>
2022-02-27 17:35:15 -0800

    Finished relations

--- buddha_base/data/books.py
+++ buddha_base/data/books.py
@@ -1,15 +1,38 @@
+from typing import List
 import sqlalchemy as sa
+import sqlalchemy.orm as orm
 
 from buddha_base.data.modelbase import SqlAlchemyBase
+from data.contributors import Contributor
 
 class Books(SqlAlchemyBase):
 
-  id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, index=True)
-  quote = sa.Column(sa.String)
+  __tablename__ = "books"
+
+  title = sa.Column(sa.Integer, primary_key=True, index=True)
+  
+  #TODO: Create scraper to populate database. 
+  description = sa.Column(sa.String)
+
+  # Author Relations:
+  authors: List[Contributor] = orm.relation("Contributor", order_by=[
+    Contributor.last_name.desc(),
+    Contributor.first_name.desc()
+  ])
+
+  editors: List[Contributor] = orm.relation("Contributor", order_by=[
+    Contributor.last_name.desc(),
+    Contributor.first_name.desc()
+  ])
+
+  translators: List[Contributor] = orm.relation("Contributor", order_by=[
+    Contributor.last_name.desc(),
+    Contributor.first_name.desc()
+  ])
+  
+
+
 
   
   
   
-  book = sa.column(sa.String)
-  author
-  #TODO: Write out more aspects for quotes.
